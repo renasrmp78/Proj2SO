95aaff360b1d53b5c1d1019cba1d17b8157d54dc
Miguel M Marques <miguel.cs.mirandamarques@gmail.com>
2025-01-12 01:23:37 +0000

    ignora a copy

---
+++ proj_SO24-25-p2 copy/src/client/api.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "api.h"
+
+#include "src/common/constants.h"
+#include "src/common/protocol.h"
+#include "src/common/io.h"
+#include "src/server/io.h"
+
+char const *req_pipe_path_c;
+char const *resp_pipe_path_c;
+char const *server_pipe_path_c;
+char const *notif_pipe_path_c;
+
+int req_fd;
+int resp_fd;
+int notif_fd;
+int server_fd;
+
+const char* OP_to_string(int op) {
+    switch (op) {
+        case OP_CODE_CONNECT: return "connect";
+        case OP_CODE_DISCONNECT: return "disconnect";
+        case OP_CODE_SUBSCRIBE: return "subscribe";
+        case OP_COPE_UNSUBSCRIBE: return "unsubscribe";
+        default: return "UNKNOWN";
+    }
+}
+
+//m “Server returned <response-code> for operation: <connect|disconnect|subscribe|unsubscribe>
+void print_answer(char ans_code, char op){
+  write_str(STDOUT_FILENO, "Server returned ");
+  write_str(STDOUT_FILENO, ans_code);
+  write_str(STDOUT_FILENO, " for operation: ");
+  write_str(STDOUT_FILENO, OP_to_string((int)op));
+  write_str(STDOUT_FILENO, "\n");
+}
+
+
+int kvs_connect(char const *req_pipe_path, char const *resp_pipe_path,
+                char const *server_pipe_path, char const *notif_pipe_path,
+                int *notif_pipe) {
+  
+  //m keep them in this file?
+  req_pipe_path_c = req_pipe_path;
+  resp_pipe_path_c = resp_pipe_path;
+  server_pipe_path_c = server_pipe_path;
+  notif_pipe_path_c = notif_pipe_path;
+  
+  // create pipes and connect
+  //m now create them
+  if(mkfifo(req_pipe_path, 0666) != 0){
+    fprintf("Failed to create fifo <%s>\n", req_pipe_path);
+    return 1;
+  }
+  if(mkfifo(resp_pipe_path, 0666) != 0){
+    fprintf("Failed to create fifo <%s>\n", resp_pipe_path);
+    return 1;
+  }
+  if(mkfifo(notif_pipe_path, 0666) != 0){
+    fprintf("Failed to create fifo <%s>\n", notif_pipe_path);
+    return 1;
+  }
+
+  //m open connections
+  //m connect to server
+  server_fd = open(server_pipe_path, O_WRONLY);
+  if (server_fd == -1) {
+    fprintf("Failed to open fifo <%s> for writing\n", server_pipe_path);
+    return 1;
+  }
+  //m We now probably need to send to the server the name of the fifos we just created             !!!
+  char buffer[1 + 40 + 40 + 40] = {'\0'};
+  
+  strncpy(buffer, "1", 1);
+
+  char path[40] = '\0';
+  strncpy(path, req_pipe_path, strlen(req_pipe_path));
+  strncpy(buffer + 1, path, 40);
+
+  memset(path, '\0', 40);
+  strncpy(path, resp_pipe_path, strlen(resp_pipe_path));
+  strncpy(buffer + 1 + 40, path, 40);
+
+  memset(path, '\0', 40);
+  strncpy(path, notif_pipe_path, strlen(notif_pipe_path));
+  strncpy(buffer + 1 + 40 + 40, path, 40);
+
+  write_all(server_fd, buffer, 1 + 40 + 40 + 40);
+  //m Until the server connects to the respective fifos we will send, our program will
+  //m bbe blocked in these next opens
+
+  //m connect to requests pipe
+  req_fd = open(req_pipe_path, O_WRONLY);
+  if (req_fd == -1) {
+    fprintf("Failed to open fifo <%s> for writing\n", req_pipe_path);
+    return 1;
+  }
+  
+  //m connect to answers pipe
+  resp_fd = open(resp_pipe_path, O_RDONLY);
+  if (resp_fd == -1) {
+    fprintf("Failed to open fifo <%s> for reading\n", resp_pipe_path);
+    return 1;
+  }
+  
+  //m connect to notifications pipe
+  notif_fd = open(notif_pipe_path, O_RDONLY);
+  if (notif_fd == -1) {
+    fprintf("Failed to open fifo <%s> for reading\n", notif_pipe_path);
+    return 1;
+  }
+  *notif_pipe = notif_fd; //m secalhar vai ser necessario
+  
+  char buff[2];
+  read_all(resp_fd, buff, 2, NULL);
+  if (buff[0] != '1'){
+    fprintf(stderr, "Problem with server feedback about connecting\n");
+    return 1;
+  }
+
+  print_answer(buff[1], buff[0]);
+
+  return 0;
+}
+
+/**m
+ * Envia um pedido de disconnect para o FIFO de pedido
+ * Feixa os named pipes que abriu no connect e 
+ * apaga os named pipe que criou
+ * 
+ * O servidor deverá eliminar todas as subscricoes deste cliente
+ */
+int kvs_disconnect(void) {
+  // close pipes and unlink pipe files
+  
+  //m I think, it needs to send a menssage to the server here                                      !!!
+
+  //m nos exemplos do lab, não verificam erro neste tipo de close
+  
+  //m Communicate with server
+  write_str(req_fd ,"2");
+  char buff[2];
+  read_all(resp_fd, buff, 2, NULL);
+
+  if (buff[0] != '2'){
+    fprintf(stderr, "Problem with server feedback about desconnecting\n");
+    return 1;
+  }
+
+  print_answer(buff[1], buff[0]);
+
+  close(server_fd);
+  close(req_fd);
+  close(resp_fd);
+  close(notif_fd);
+
+  if(unlink(req_pipe_path_c) != 0){
+    fprintf("Failed to destroy fifo <%s>\n", req_pipe_path_c);
+    return 1;
+  }
+
+  if(unlink(resp_pipe_path_c) != 0){
+    fprintf("Failed to destroy fifo <%s>\n", resp_pipe_path_c);
+    return 1;
+  }
+
+  if(unlink(notif_pipe_path_c) != 0){
+    fprintf("Failed to destroy fifo <%s>\n", notif_pipe_path_c);
+    return 1;
+  }
+
+  return 0;
+
+}
+
+/**
+ * @returns 0 if the doesnt exist, 1 if existed 
+ */
+int kvs_subscribe(const char *key) {
+  // send subscribe message to request pipe and wait for response in response
+  //m lets try
+  write_str(req_fd, "3");
+  write_all(req_fd ,key, 41);
+  char buff[2];
+  read_all(resp_fd, buff, 2, NULL);
+
+  if (buff[0] != '3'){
+    fprintf(stderr, "Problem with server feedback about subscribing key\n");
+    return 1;
+  }
+
+  print_answer(buff[1], buff[0]);
+  
+  return 0;
+}
+
+/**
+ * @returns 0 if the subsctiption existed and wassuccesfuly removed, 1 case it didnt exist 
+ */
+int kvs_unsubscribe(const char *key) {
+  // send unsubscribe message to request pipe and wait for response in response
+  // pipe
+
+  //m lets try
+  write_str(req_fd, "4");
+  write_all(req_fd ,key, 41);
+  char buff[2];
+  read_all(resp_fd, buff, 2, NULL);
+
+  if (buff[0] != '4'){
+    fprintf(stderr, "Problem with server feedback about subscribing key\n");
+    return 1;
+  }
+
+  print_answer(buff[1], buff[0]);
+
+  return 0;
+}
---
+++ proj_SO24-25-p2 copy/src/client/main.c
@@ -0,0 +1,158 @@
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "parser.h"
+#include "src/client/api.h"
+#include "src/common/constants.h"
+#include "src/common/io.h"
+#include "src/server/io.h"
+
+static void *track_notif(void *arg){
+  int notif_fd = *(int*)arg;
+  char key[41], value[41];
+  int intr;
+
+  while (1){
+    int value = 0;
+    value = read_all(notif_fd, key, 41, &intr);
+    if (value == 0){break;}
+    else if (value == -1){
+      fprintf(stderr, "Error with read all while reading notifications\n");
+    }
+    else if (intr == 1){
+      fprintf(stderr, "Reading in the notification was inturupted\n");
+    }
+
+    value = read_all(notif_fd, value, 41, &intr);
+    if(value == 0 || value == -1){
+      printf("Error while reading from notifications\n");
+    }
+    else if (intr == 1){
+      fprintf(stderr, "Reading in the notification was inturupted\n");
+    }
+
+    write_str(STDOUT_FILENO, "(");
+    write_str(STDOUT_FILENO, key);
+    write_str(STDOUT_FILENO, ",");
+    write_str(STDOUT_FILENO, value);
+    write_str(STDOUT_FILENO, ")\n");
+  }
+
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s <client_unique_id> <register_pipe_path>\n",
+            argv[0]);
+    return 1;
+  }
+  char *server_pipe_path = argv[1];
+
+  char req_pipe_path[256] = "/tmp/req";
+  char resp_pipe_path[256] = "/tmp/resp";
+  char notif_pipe_path[256] = "/tmp/notif";
+  int req_fd = 0, resp_fd = 0, notif_fd = 0;
+
+  char keys[MAX_NUMBER_SUB][MAX_STRING_SIZE] = {0};
+  unsigned int delay_ms;
+  size_t num;
+
+  strncat(req_pipe_path, argv[1], strlen(argv[1]) * sizeof(char));
+  strncat(resp_pipe_path, argv[1], strlen(argv[1]) * sizeof(char));
+  strncat(notif_pipe_path, argv[1], strlen(argv[1]) * sizeof(char));
+
+  
+  // TODO open pipes
+  
+  
+  int notif_pipe;
+  if (kvs_connect(req_pipe_path, resp_pipe_path, server_pipe_path,
+      notif_pipe_path, &notif_pipe) != 0){
+    fprintf(stderr, "Failed to connect to server\n");
+    return 1;
+  }
+
+  //m still have to create threads to do the operations that
+  //m are responsible for receiving the notifications and sending
+  //m it to the stdout: I thing notif_pipe is for here
+  //m thread_func
+  pthread_t notif_thread;
+  if (pthread_create(&notif_thread, NULL, track_notif, (void *)&notif_pipe) !=
+      0) {
+    fprintf(stderr, "Failed to create notifications thread\n");
+  }
+
+  //m acho que a tarefa principal deverá ficar a realizar este
+  //m while, tmb deve fazer os respetivos pedidos ao server
+  //m e receber as respetivas respostas
+  while (1) {
+
+    switch (get_next(STDIN_FILENO)) {
+    case CMD_DISCONNECT:
+      if (kvs_disconnect() != 0) {
+        fprintf(stderr, "Failed to disconnect to the server\n");
+        return 1;
+      }
+      // TODO: end notifications thread
+      //m lets put the join i think
+      if (pthread_join(notif_thread, NULL) != 0) {
+        fprintf(stderr, "Failed to join notification thread\n");
+      }
+      printf("Disconnected from server\n");
+      return 0;
+
+    case CMD_SUBSCRIBE:
+      num = parse_list(STDIN_FILENO, keys, 1, MAX_STRING_SIZE);
+      if (num == 0) {
+        fprintf(stderr, "Invalid command. See HELP for usage\n");
+        continue;
+      }
+
+      if (kvs_subscribe(keys[0])) {
+        fprintf(stderr, "Command subscribe failed\n");
+      }
+
+      break;
+
+    case CMD_UNSUBSCRIBE:
+      num = parse_list(STDIN_FILENO, keys, 1, MAX_STRING_SIZE);
+      if (num == 0) {
+        fprintf(stderr, "Invalid command. See HELP for usage\n");
+        continue;
+      }
+
+      if (kvs_unsubscribe(keys[0])) {
+        fprintf(stderr, "Command subscribe failed\n");
+      }
+
+      break;
+
+    case CMD_DELAY:
+      if (parse_delay(STDIN_FILENO, &delay_ms) == -1) {
+        fprintf(stderr, "Invalid command. See HELP for usage\n");
+        continue;
+      }
+
+      if (delay_ms > 0) {
+        printf("Waiting...\n");
+        delay(delay_ms);
+      }
+      break;
+
+    case CMD_INVALID:
+      fprintf(stderr, "Invalid command. See HELP for usage\n");
+      break;
+
+    case CMD_EMPTY:
+      break;
+
+    case EOC:
+      // input should end in a disconnect, or it will loop here forever
+      break;
+    }
+  }
+}
